7c5111007c8304c031e9338617513eb985973528
Spotika4 <kobtsev.sasha@yandex.ru>
2026-02-06 20:55:25 +0300

    commit

---
+++ framework/frontend/assets/AppAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+
+class AppAsset extends AssetBundle {
+	
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    
+    public $css = [
+        'css/main.css',
+    ];
+    
+    public $js = [
+    ];
+    
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+	
+	public $jsOptions = [
+		'position' => \yii\web\View::POS_HEAD
+	];
+}
